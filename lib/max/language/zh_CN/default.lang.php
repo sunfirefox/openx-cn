ae0f4d1e2b799e840bd89cba2f5933806e4f6d25
esaalii <sah.lee@ericsson.com>
2013-04-21 13:58:26 +0800

    Revert "Date Formats"
    This reverts commit cfc48cc2fb119f612f53ab2d53285456dfcc3d15.

--- lib/max/language/zh_CN/default.lang.php
+++ lib/max/language/zh_CN/default.lang.php
@@ -35,11 +35,11 @@ $GLOBALS['phpAds_DecimalPoint']         = ".";
 $GLOBALS['phpAds_ThousandsSeperator']   = ",";
 
 // Date & time configuration
-$GLOBALS['date_format']                 = "%Y-%mm-%d";
+$GLOBALS['date_format']                 = "%Y-%m-%d";
 $GLOBALS['time_format']                 = "%H:%M:%S";
 $GLOBALS['minute_format']               = "%H:%M";
-$GLOBALS['month_format']                = "%Y-%mm";
-$GLOBALS['day_format']                  = "%mm-%d";
+$GLOBALS['month_format']                = "%Y-%m";
+$GLOBALS['day_format']                  = "%d-%m";
 $GLOBALS['week_format']                 = "%Y-%W";
 $GLOBALS['weekiso_format']              = "%V-%G";
 
